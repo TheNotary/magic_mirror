0f8876d1eb536652431e8615279c8bdc4b03384d
TheNotary <no@email.plz>
2015-08-20 13:36:56 -0500

    completed /missing_cache api so clients can request the set of the command_cache they will likely miss

--- lib/magic_mirror/sinatra_silver.rb
+++ lib/magic_mirror/sinatra_silver.rb
@@ -4,10 +4,8 @@ module SinatraSilver
   class App < Sinatra::Base
     set :root, MagicMirror.sinatra_root
     # set :app_file, MagicMirror.sinatra_root
-    # set :views, MagicMirror.sinatra_root
     set :views, Proc.new { File.join(MagicMirror.sinatra_root, "lib", "views") }
     #set :public_folder, Proc.new { File.join(MagicMirror.sinatra_root, "lib", "views") }
-    #set :logging, false
     set :logging, false
     set :bind, '127.0.0.1'
 
@@ -19,16 +17,21 @@ module SinatraSilver
       @command_cache = MagicMirror.command_cache.to_json
       @command_cache_length = MagicMirror.command_cache.length
       @command_cache_fast_evaluator = MagicMirror.command_cache.to_embedded_javascript
-      erb :index # 'Hello world!'
+      erb :index
     end
 
     get '/webworker.js' do
       erb :webworker
     end
 
+    # There's a gap in the client's command cache, right after page load and
+    # right before faye is connected, you will be missing commands.
+    # This action is called when faye is connected to fill in the gap.
     get '/missing_cache' do
-      
+      l = params[:command_cache_length].to_i
+      start_index = l-1
 
+      MagicMirror.command_cache[start_index..-1].to_json
     end
   end
 end
